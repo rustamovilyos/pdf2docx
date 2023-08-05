bdc2299d8dad1df749b827b3a592014f5e58b79b
rustamovilyos <rustamov7871@gmail.com>
2023-08-06 02:46:48 +0500

    updated

--- interface.py
+++ interface.py
@@ -1,11 +1,14 @@
 import io
 import sys
+import threading
 from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 import PyPDF2
 from typing import Tuple
 from tkinter import messagebox as mbox
+
+from pdf2docx import parse
 
 
 class Interface(Frame):
@@ -14,18 +17,17 @@ class Interface(Frame):
         super().__init__()
         self.upload()
 
-    def open_file(self):
-        """Открывает файл для редактирования"""
-        filepath = askopenfilename(
-            filetypes=[("Text Files", "*.pdf")]
-        )
-        print(filepath)
-        return self.convert_pdf2docx(f'{filepath}', output_file=f'{filepath}.docx')
-
     def upload(self):
         btn_open = ttk.Button(text="Выбрать файл", command=self.open_file)
 
-        btn_open.grid(row=0, column=0, sticky="ew", padx=100, pady=50)
+        btn_open.pack(expand=True, padx=15, pady=15)
+
+    def show_conversion_animation(self):
+        self.label_animation = Label(self, text="идет...")
+        self.label_animation.pack(expand=True, padx=20, pady=5, fill=BOTH)
+
+    def stop_conversion_animation(self):
+        self.label_animation.pack_forget()
 
     def get_num_pages(self, input_file: str) -> int:
         """Get number of pages in the PDF"""
@@ -35,51 +37,80 @@ class Interface(Frame):
 
     def convert_pdf2docx(self, input_file: str, output_file: str, pages: Tuple = None):
         """Converts pdf to docx"""
-        # Redirecting print to a StringIO object
-        output_buffer = io.StringIO()
-        sys.stdout = output_buffer
+        try:
+            self.show_conversion_animation()
+            # if self.show_conversion_animation():
+            #     self.stop_conversion_animation()
+            # else:
+            #     self.show_conversion_animation()
+            # Redirecting print to a StringIO object
+            output_buffer = io.StringIO()
+            sys.stdout = output_buffer
 
-        summary = {
-            "Файл": input_file, "Страницы": self.get_num_pages(input_file), "Выходной файл": output_file
-        }
+            if pages:
+                pages = [int(i) for i in list(pages) if i.isnumeric()]
+            result = parse(pdf_file=input_file,
+                           docx_with_path=output_file, pages=pages)
+            try:
+                summary = {
+                    "Файл": input_file, "Страницы": self.get_num_pages(input_file), "Выходной файл": output_file
+                }
+            except FileNotFoundError:
+                self.onError_FileNotFoundError()
 
-        # Restoring sys.stdout
-        sys.stdout = sys.__stdout__
+            # Restoring sys.stdout
+            try:
+                sys.stdout = sys.__stdout__
 
-        # Printing Summary
-        output_text = output_buffer.getvalue()
-        self.text_output.config(state=NORMAL)
-        self.text_output.delete(1.0, END)
-        self.text_output.insert(END, "\n".join(f"{i}:{j}" for i, j in summary.items()))
-        self.text_output.insert(END, output_text)
-        self.text_output.config(state=DISABLED)
+                # Printing Summary
+                output_text = output_buffer.getvalue()
+                self.text_output.config(state=NORMAL)
+                self.text_output.delete(1.0, END)
+                self.text_output.insert(END, "\n".join(f"{i}:{j}" for i, j in summary.items()))
+                self.text_output.insert(END, output_text)
+                self.text_output.config(state=DISABLED)
 
-        # Close the StringIO object
-        output_buffer.close()
+                # Close the StringIO object
+                output_buffer.close()
 
-        return self.onInfo()
+                return self.onInfo()
+            except UnboundLocalError:
+                self.onError_UnboundLocalError()
+
+        except KeyboardInterrupt:
+            return self.onError()
 
     def onError(self):
-        mbox.showerror("Ошибка", "Не могу открыть файл")
+        mbox.showerror("Ошибка!!!", "Не могу открыть файл!")
+
+    def onError_FileNotFoundError(self):
+        mbox.showerror("Ошибка!!!", "Файл не выбран!")
+
+    def onError_UnboundLocalError(self):
+        mbox.showwarning("Внемание!", "Пожалуйста выбирайте .pdf файл!")
 
     def onInfo(self):
-        mbox.showinfo("Информация", "Конвертация завершено")
+        mbox.showinfo("Информация", "Конвертация завершено!")
+
+    def open_file(self):
+        """Открывает файл для редактирования"""
+        filepath = askopenfilename(
+            filetypes=[("Text Files", "*.pdf"), ("All files", "*")]
+        )
+        t = threading.Thread(target=self.convert_pdf2docx, args=(filepath, f'{filepath}.docx'))
+        t.start()
 
 
 def main():
     window = Tk()
     user = Interface()
     window.title("Добро пожаловать в приложение")
-    window.geometry('300x150+500+300')
+    window.geometry('680x330+350+100')
 
-    # Create a Toplevel window for text output
-    text_window = Toplevel(window)
-    text_window.title("Результаты конвертации")
-    text_window.geometry("600x400+600+200")
 
     # Create a Text widget for text output
-    user.text_output = Text(text_window, wrap=WORD, state=DISABLED)
-    user.text_output.pack(expand=True, fill=BOTH)
+    user.text_output = Text(window, wrap=WORD)
+    user.text_output.pack(expand=True, padx=20, pady=15, fill=BOTH)
 
     window.mainloop()
 
